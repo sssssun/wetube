790a95236c275944ccfe3a3444073d1c3e24e795
박선영 <sunsmile01023@gmail.com>
2019-04-17 11:30:19 +0900

    join을 getJoin, postJoin으로 분리, 비밀번호 일치 여부에 따라 400에러 or home으로 redirect

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,29 @@
-export const join=(req , res) => res.render("join");
+import routes from "../routes";
+
+export const getJoin=(req , res) => {
+    res.render("Join", {pageTitle:"Join"});
+}
+
+export const postJoin=(req, res) => {
+    console.log(req.body);
+    const joinInfo= {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        password2 : req.body.password2
+    }
+
+    if(joinInfo.password!==joinInfo.password2){
+        res.status(400);
+        res.render("Join", {pageTitle:"Join", joinInfo});
+    }
+    else{
+        res.redirect(routes.home);
+    }
+    
+    
+}
+
 export const login=(req , res) => res.render("login");
 export const logout=(req , res) => res.render("logout");
 export const users=(req,res) => res.render("users");
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -7,7 +7,7 @@ export const home=(req , res) => {
 
 export const search=(req , res) => {
     const searchingBy = req.query.term;
-    res.render("search", {pageTitle : "Search", searchingBy : searchingBy});
+    res.render("search", {pageTitle : "Search", searchingBy : searchingBy, videos});
 };
 
 export const upload=(req, res) => res.render("upload", {pageTitle : "Upload"});
