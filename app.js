f1c9822bffcdf9b4ef0b52f4f9ae5511e768a0ca
박선영 <sunsmile01023@gmail.com>
2019-04-23 00:20:51 +0900

    middlewares.js에서 경로 바꾸고, app.js에서 express.static 으로 파일 전달, home.pug에서 videoFile을 fileUrl로 바궈야함 주의

--- app.js
+++ app.js
@@ -7,28 +7,21 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 
 const app = express();
 
 
 app.set("view engine","pug");
+app.use("/uploads",express.static("uploads"));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function(req, res, next) {
-    res.locals.siteName="WeTube";
-    res.locals.routes=routes;
-    res.locals.user={
-        isAuthenticated: true,
-        id: 1
-    };
-    next();
-
-});
+app.use(localsMiddleware);
 
 app.use(routes.home,globalRouter);
 app.use(routes.users,userRouter);
