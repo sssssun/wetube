60690be2b7e7cf5faf9d4bf00996ceb7be280914
박선영 <sunsmile01023@gmail.com>
2019-05-01 20:48:58 +0900

    webpack 설치 및 설정

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path=require("path");
+const autoprefixer=require("autoprefixer");
+const ExtractText = require("extract-text-webpack-plugin");
+
+//entry point : 어떤 모듈을 사용해서 시작할지, output: 만들어진 파일 어떤 파일명, 어떤 폴더에 넣을지
+const ENTRY_FILE=path.resolve(__dirname, "assets","js","main.js");
+const OUTPUT_DIR=path.join(__dirname,"static");
+
+module.exports={
+    entry: ENTRY_FILE,
+    mode: process.env.WEBPACK_ENV,
+    module: {
+        rules: [
+            {
+                test:/\.scss$/,
+                use: ExtractText.extract(
+                    [
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader:"postcss-loader",
+                        options: {
+                            plugins(){
+                                return [autoprefixer({browsers:"cover 99.5%"})];
+                            }
+                        }
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+                    
+                )
+                
+                
+            }
+        ]
+    },
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[name].js"
+    },
+    plugins: [
+        new ExtractText("styles.css")
+        
+    ]
+    
+};
